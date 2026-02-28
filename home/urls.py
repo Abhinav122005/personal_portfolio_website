651339068b8a41818b86193c6ab986ff47133dc1
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-02-28 18:14:52 +0530

    added resume

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path("resume/", views.resume, name="resume"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from projects.models import Project
 def home_view(request):
     projects = Project.objects.all().order_by('-created_at')[:3]
     return render(request, 'home/index.html', {'projects': projects})
+
+def resume(request):
+    return render(request, "home/resume.html")
